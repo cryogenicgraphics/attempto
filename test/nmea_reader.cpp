42f24f450a645d98da9875fb505516abe837c6d0
Goncalo Carvalho <glslang@gmail.com>
2016-01-29 23:44:39 +0000

    CB-1 - change of name

--- test/nmea_reader.cpp
+++ test/nmea_reader.cpp
@@ -3,20 +3,20 @@
 //
 
 /*
-    This file is part of Reliving.
+    This file is part of Attempto.
 
-    Reliving is free software: you can redistribute it and/or modify
+    Attempto is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Reliving is distributed in the hope that it will be useful,
+    Attempto is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Reliving.  If not, see <http://www.gnu.org/licenses/>.
+    along with Attempto.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifdef __clang__
